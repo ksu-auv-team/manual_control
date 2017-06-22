c37de486473ccd248a5d0ff9044bc38a00b1cd4b
ground-user <vnguye34@students.kennesaw.edu>
2017-06-22 17:44:00 -0400

    modified name, added setup

--- joystick_setup.sh
+++ joystick_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 15
-sudo chmod a+rw /dev/input/js0
 rosparam set joy_node/dev "/dev/input/js0"
-rosrun manual_control manual_control
+echo "joystick setup"
+rosrun joy joy_node
+echo "joystick publishing"
